75d4f419f0ce9b17cdfce0ef347640775913a793
Catherine Bronzo <cbronzo@mac.com>
2018-08-11 22:50:12 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,4 +1,5 @@
 class Helpers
+  
   def self.current_user(session)
     @user = User.find_by_id(session[:user_id])
   end
@@ -6,4 +7,5 @@ class Helpers
   def self.is_logged_in?(session)
     !!session[:user_id]
   end
+  
 end
